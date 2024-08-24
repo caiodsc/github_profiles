49144be476ab1d1f39eb03df52d2123b682d4a9e
caiodsc <caio.dscamara@gmail.com>
2024-08-24 16:32:42 -0400

    feature: add state to users

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,21 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_824_192_200) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_24_203142) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'github_url'
-    t.string 'github_name'
-    t.integer 'followers'
-    t.integer 'following'
-    t.integer 'stars'
-    t.integer 'contributions_last_year'
-    t.string 'profile_image_url'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['github_url'], name: 'index_users_on_github_url', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "github_url"
+    t.string "github_name"
+    t.integer "followers"
+    t.integer "following"
+    t.integer "stars"
+    t.integer "contributions_last_year"
+    t.string "profile_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "state"
+    t.index ["github_url"], name: "index_users_on_github_url", unique: true
   end
+
 end
