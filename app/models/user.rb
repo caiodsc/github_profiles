92010bda8b48f0d04fa1a1aac2e15c2259e27502
caiodsc <caio.dscamara@gmail.com>
2024-08-25 13:59:04 -0400

    feature: define state machine for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@
 class User < ApplicationRecord
   validates_presence_of :name, :github_url
   validates_uniqueness_of :github_url
+
+  after_create :start_processing, :enqueue_processing_job
+
+  PENDING_STATE = :pending
+  PROCESSING_STATE = :processing
+  COMPLETED_STATE = :completed
+  FAILED_STATE = :failed
+
+  state_machine :state, initial: :pending do
+    state PENDING_STATE
+    state PROCESSING_STATE
+    state COMPLETED_STATE
+    state FAILED_STATE
+
+    event :start_processing do
+      transition pending: :processing
+    end
+  end
+
+  def enqueue_processing_job
+    GithubScraperJob.perform_later(id)
+  end
 end
