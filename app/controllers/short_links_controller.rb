c8ddd09b2a277319c5f83bedad16279638cdf587
caiodsc <caio.dscamara@gmail.com>
2024-08-26 13:54:36 -0400

    feature: implement short links controller and route

---
+++ app/controllers/short_links_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ShortLinksController < ApplicationController
+  before_action :set_user, only: :show
+
+  def show
+    redirect_to @user.github_url, allow_other_host: true
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(unique_identifier:)
+  end
+
+  def unique_identifier
+    ShortCode.decode(params[:id])
+  end
+end
