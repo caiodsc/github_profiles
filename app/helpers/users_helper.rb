12d66bfd0c87a9a93174798ed471e925b7b57030
caiodsc <caio.dscamara@gmail.com>
2024-08-26 13:02:30 -0400

    feature: create shortened_github_url helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def shortened_github_url(user)
+    Rails.application.config.url + "/s/#{ShortCode.encode(user.unique_identifier)}"
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Development host url
+  config.url = 'https://localhost:3000'
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
