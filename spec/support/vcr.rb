60f1c57aa2e4518a54bed4b05c31bda5d5bb5792
caiodsc <caio.dscamara@gmail.com>
2024-08-26 21:54:12 -0400

    refactor: configure vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,6 +5,4 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr'
   config.hook_into :webmock
-  config.configure_rspec_metadata!
-  config.ignore_localhost = true
 end
