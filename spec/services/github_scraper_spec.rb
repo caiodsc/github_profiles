2bbaad36093a48fea27b23b7513229b97c84146b
caiodsc <caio.dscamara@gmail.com>
2024-08-26 23:52:07 -0400

    refactor: refactor github_scraper_spec.rb

--- spec/services/github_scraper_spec.rb
+++ spec/services/github_scraper_spec.rb
@@ -8,18 +8,23 @@ RSpec.describe GithubScraper do
 
   describe '#user_info' do
     subject { scraper.user_info }
+    let(:expected_user_info) do
+      {
+        github_name: 'matz',
+        followers: '9.7k',
+        following: '1',
+        stars: '17',
+        contributions_last_year: '757',
+        profile_image_url: 'https://avatars.githubusercontent.com/u/30733?v=4',
+        location: 'Matsue, Japan',
+        organization: 'Ruby Association,NaCl'
+      }
+    end
 
     context 'with a valid GitHub profile' do
       it 'returns the correct metadata' do
         VCR.use_cassette('github_profile') do
-          expect(subject).to eq({ github_name: 'matz',
-                                  followers: '9.7k',
-                                  following: '1',
-                                  stars: '17',
-                                  contributions_last_year: '757',
-                                  profile_image_url: 'https://avatars.githubusercontent.com/u/30733?v=4',
-                                  location: 'Matsue, Japan',
-                                  organization: 'Ruby Association,NaCl' })
+          expect(subject).to eq(expected_user_info)
         end
       end
     end
